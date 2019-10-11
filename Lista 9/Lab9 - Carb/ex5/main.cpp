8a3af25856616dacc9639ee9abcf81cde48fc017
Lufec <30414428+Lufec@users.noreply.github.com>
2019-10-10 21:58:53 -0300

    ex5

--- Lista 9/Lab9 - Carb/ex5/main.cpp
+++ Lista 9/Lab9 - Carb/ex5/main.cpp
@@ -2,30 +2,10 @@
 
 using namespace std;
 
-void descer(int H[],int i, int n){
-    int j= 2*i+1; // filho esquerdo
-    if(j<=n){
-        if(j<n){
-            if(H[j+1]>H[j]){
-                j++;
-            }
-            cout<<"Checkpoint 1 descer "<<j<<endl;
-        }
-        if(H[i]<H[j]){
-            int aux = H[i];
-            H[i]=H[j];
-            H[j]=aux;
-            cout<<"Checkpoint 2 descer "<<j<<endl;
-            descer(H,j,n);
-        }
-    }
-}
-
-void arranjar(int H[],int n){
-    for(int i=(n+1)/2;i>=0;i--){
-        cout<<"Checkpoint 1 arranjar "<<i<<endl;
-        descer(H,i,n);
-    }
+void troca(int &x, int &y){
+    int aux=x;
+    x=y;
+    y=aux;
 }
 
 void printH(int H[], int n){
@@ -35,40 +15,86 @@ void printH(int H[], int n){
     cout<<endl;
 }
 
-void HeapSort(int H[],int n){
-    arranjar(H,n);
-    cout<<"Checkpoint 1 heap"<<endl;
-    int m=n-1;
-    printH(H,n);
-    while(m>0){
-        int aux = H[0];
-        H[0] = H[m];
-        H[m] = aux;
-        cout<<"Checkpoint 2 heap "<<m-1<<endl;
-        m--;
-        printH(H,n);
-        descer(H,0,m);
+void descer(int D[],int V[], int P[],int i, int n){
+    int j= 2*i+1; // filho esquerdo
+    if(j<=n){
+        if(j<n){
+            if(D[j]>D[j-1]){
+                j++;
+            }
+        }
+        if(D[i]<D[j-1]){
+            troca(D[i],D[j-1]);
+            troca(V[i],V[j-1]);
+            troca(P[i],P[j-1]);
+            descer(D,V,P,j-1,n);
+        }
     }
+}
+
+void arranjar(int D[],int V[],int P[],int n){
+    for(int i=(n+1)/2;i>=0;i--){
+        descer(D,V,P,i,n);
+    }
+}
+
+int roubo(int V[], int P[], int m, int n){
+    int D[n];
+    for(int i=0;i<n;i++){
+        D[i]=V[i]/P[i];
+    }
+    int total=0;
+    int valor=0;
+    while(total<m){
+        arranjar(D,V,P,n);
+        printH(D,n);
+        printH(V,n);
+        printH(P,n);
+        cout<<endl;
+        valor+=V[0];
+        total+=P[0];
+        D[0]=0;
+    }
+
+    if(total>m){
+        int dif=total-m;
+        valor-= (dif*V[0]/P[0]);
+
+    }
+    return valor;
+
+
 }
 
 
 int main()
 {
     int n;
+    cout<<"Numero de barras"<<endl;
     cin>>n;
 
-    int H[n];
+    cout<<"Valor das barras"<<endl;
+    int V[n];
 
     for(int i=0;i<n;i++){
-        cin>>H[i];
+        cin>>V[i];
     }
 
-    HeapSort(H,n);
+    cout<<"Peso das barras"<<endl;
+    int P[n];
 
-    printH(H,n);
+    for(int i=0;i<n;i++){
+        cin>>P[i];
+    }
+
+    int m;
+    cout<<"capacidade da mala"<<endl;
+    cin>>m;
 
     cout<<endl;
 
+    int valor = roubo(V,P,m,n);
 
+    cout<<"valor roubado = "<<valor<<endl;
     return 0;
 }
